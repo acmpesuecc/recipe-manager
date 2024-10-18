ea9c1c265d58d471b111fbbd0461981ed48aa777
Whizzy <151893825+stealthwhizz@users.noreply.github.com>
2024-10-19 01:25:36 +0530

    Update RecipeController.java

--- src/main/java/com/example/HelloSQL/RecipeController.java
+++ src/main/java/com/example/HelloSQL/RecipeController.java
@@ -1,5 +1,8 @@
 package com.example.HelloSQL;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
