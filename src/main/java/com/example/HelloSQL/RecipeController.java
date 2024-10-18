5c1be856e14ff982350b428820353d053985583a
Whizzy <stealthwhiz@gmail.com>
2024-10-19 01:23:14 +0530

    updated one

--- src/main/java/com/example/HelloSQL/RecipeController.java
+++ src/main/java/com/example/HelloSQL/RecipeController.java
@@ -1,9 +1,18 @@
 package com.example.HelloSQL;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user/recipe")
@@ -30,6 +39,13 @@ public class RecipeController {
     public void rateRecipe(@PathVariable Long id, @RequestParam double rating) {
         recipeService.rateRecipe(id, rating);
     }
+    
+    @GetMapping("/view")
+public String viewRecipes(Model model) {
+    List<RecipeModel> recipes = recipeService.getAllRecipes();
+    model.addAttribute("recipes", recipes);
+    return "viewRecipes";
+}
 
 
     @DeleteMapping("/delete/{id}")
